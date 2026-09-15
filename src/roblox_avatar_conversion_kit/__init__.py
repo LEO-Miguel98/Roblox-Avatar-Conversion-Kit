00fdd7b311de4842e38c8e7e5df91f39d41bcce2
LEO-Miguel98 <leotvgamingt@gmail.com>
2026-09-16 01:35:02 +0800

    v0.3.24: install face surface binding finalizer

--- src/roblox_avatar_conversion_kit/__init__.py
+++ src/roblox_avatar_conversion_kit/__init__.py
@@ -1,6 +1,6 @@
 """Roblox Avatar Conversion Kit."""
 
-__version__ = "0.3.23"
+__version__ = "0.3.24"
 
 # Keep facial reconstruction isolated from the binary PMX writer while still letting write_pmx use
 # the latest face-region logic. Importing the package installs the extensions once after pmx loads.
@@ -11,6 +11,7 @@ from .face_boundary import install as _install_face_boundary
 from .face_motion_safe import install as _install_face_motion_safe
 from .face_aperture_lock import install as _install_face_aperture_lock
 from .face_island_stability import install as _install_face_island_stability
+from .face_surface_bind import install as _install_face_surface_bind
 
 _install_face_runtime(_pmx)
 _install_face_follow(_pmx)
@@ -18,9 +19,11 @@ _install_face_boundary(_pmx)
 _install_face_motion_safe(_pmx)
 _install_face_aperture_lock(_pmx)
 _install_face_island_stability(_pmx)
+_install_face_surface_bind(_pmx)
 del _install_face_runtime
 del _install_face_follow
 del _install_face_boundary
 del _install_face_motion_safe
 del _install_face_aperture_lock
 del _install_face_island_stability
+del _install_face_surface_bind
