681a7258f96103e354d3773bea18431a4affbe45
LEO-Miguel98 <leotvgamingt@gmail.com>
2026-09-16 00:53:53 +0800

    v0.3.22: install aperture-lock finalizer

--- src/roblox_avatar_conversion_kit/__init__.py
+++ src/roblox_avatar_conversion_kit/__init__.py
@@ -1,6 +1,6 @@
 """Roblox Avatar Conversion Kit."""
 
-__version__ = "0.3.21"
+__version__ = "0.3.22"
 
 # Keep facial reconstruction isolated from the binary PMX writer while still letting write_pmx use
 # the latest face-region logic. Importing the package installs the extensions once after pmx loads.
@@ -9,12 +9,15 @@ from .face_runtime import install as _install_face_runtime
 from .face_follow import install as _install_face_follow
 from .face_boundary import install as _install_face_boundary
 from .face_motion_safe import install as _install_face_motion_safe
+from .face_aperture_lock import install as _install_face_aperture_lock
 
 _install_face_runtime(_pmx)
 _install_face_follow(_pmx)
 _install_face_boundary(_pmx)
 _install_face_motion_safe(_pmx)
+_install_face_aperture_lock(_pmx)
 del _install_face_runtime
 del _install_face_follow
 del _install_face_boundary
 del _install_face_motion_safe
+del _install_face_aperture_lock
