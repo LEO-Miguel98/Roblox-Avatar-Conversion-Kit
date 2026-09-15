d90d50d47cf11de67ea4b099785eac0da907b766
LEO-Miguel98 <leotvgamingt@gmail.com>
2026-09-15 16:26:35 +0800

    test: protect primary body weights

--- tests/test_weights.py
+++ tests/test_weights.py
@@ -1,7 +1,7 @@
 import unittest
 
 from roblox_avatar_conversion_kit.rig import Bone
-from roblox_avatar_conversion_kit.weights import weights_for_point
+from roblox_avatar_conversion_kit.weights import PRIMARY_WEIGHT_FLOORS, weights_for_point
 
 
 class WeightTests(unittest.TestCase):
@@ -10,16 +10,25 @@ class WeightTests(unittest.TestCase):
             Bone("hips", None, (0, 0, 0)),
             Bone("spine", "hips", (0, 1, 0)),
             Bone("chest", "spine", (0, 2, 0)),
+            Bone("neck", "chest", (0, 2.5, 0)),
+            Bone("head", "neck", (0, 3, 0)),
             Bone("leftUpperArm", "chest", (1, 2, 0)),
             Bone("leftLowerArm", "leftUpperArm", (2, 2, 0)),
             Bone("leftHand", "leftLowerArm", (3, 2, 0)),
         ]
 
-    def test_weights_normalize_and_blend(self):
+    def test_weights_normalize_blend_and_keep_primary_floor(self):
         weights = weights_for_point((1.9, 2, 0), "leftUpperArm", self.bones)
         self.assertAlmostEqual(sum(weight.weight for weight in weights), 1.0, places=6)
         self.assertGreaterEqual(len(weights), 2)
-        self.assertIn("leftLowerArm", {weight.bone for weight in weights})
+        by_name = {weight.bone: weight.weight for weight in weights}
+        self.assertIn("leftLowerArm", by_name)
+        self.assertGreaterEqual(by_name["leftUpperArm"], PRIMARY_WEIGHT_FLOORS["leftUpperArm"])
+
+    def test_head_stays_almost_rigid(self):
+        weights = weights_for_point((0, 2.6, 0), "head", self.bones)
+        by_name = {weight.bone: weight.weight for weight in weights}
+        self.assertGreaterEqual(by_name["head"], 0.97)
 
     def test_max_four_influences(self):
         self.assertLessEqual(len(weights_for_point((0, 1, 0), "spine", self.bones)), 4)
